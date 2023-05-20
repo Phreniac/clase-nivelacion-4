6d61d6a4a5383f4d20f8378e1378da96a981cee7
Juan Valdebenito <valdebenitovaldiviajuan@gmail.com>
2023-05-20 10:06:20 -0400

    prueba de commit

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,24 +1,23 @@
 let maxclick = 100;
-const button_start = document.getElementById('button_start');
+const button_start = document.getElementById("button_start");
 
-function CountClicks(){
-    if(maxclick > 0) 
-        maxclick--,
-        helpMe();
-    else 
-        showImage();
+function CountClicks() {
+  if (maxclick > 0) maxclick--, helpMe();
+  else showImage();
 }
 
-function helpMe(){
-    try {
-        //const instructions = 'Crea un elemento <img> con esta direccion relativa ../img/haciaejercicio1.png';
-        console.log(instructions);
-    } catch (error) {
-        console.log('help!!! home.js');
-    }
+function helpMe() {
+  try {
+    const instructions =
+      "Crea un elemento <img> con esta direccion relativa ../img/haciaejercicio1.png";
+    console.log(instructions);
+  } catch (error) {
+    console.log("help!!! home.js");
+  }
 }
 
-const showImage = () =>{
-    const image_container = document.getElementsByClassName('container-img');
-    if(image_container.style.display == "none") image_container.style.display = "block";
-}
+const showImage = () => {
+  const image_container = document.getElementsByClassName("container-img");
+  if (image_container.style.display == "none")
+    image_container.style.display = "block";
+};
